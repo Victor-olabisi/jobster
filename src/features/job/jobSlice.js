104b73a8c13c3b2821def6b23d06b2468a2d7227
victor <oguntayovictor65@gmail.com>
2023-11-20 17:02:04 -0800

    fixed add jobs  bugs

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import customFetch from "../../utils/customFetch";
 import { toast } from "react-toastify";
+import { logoutUser } from "../user/userSlice";
 
 const initialState = {
   isLoading: false,
@@ -20,9 +21,11 @@ export const addJobs = createAsyncThunk("addJobs", async (user, thunkAPI) => {
     const resp = await customFetch.post("/jobs", user, {
       headers: { Authorization: `Bearer ${thunkAPI.getState().user.user.token}` },
     });
+      thunkAPI.dispatch(clearValues())
       return resp.data
   } catch (error) {
       if (error.response === 401) {
+          thunkAPI.dispatch(logoutUser())
           return thunkAPI.rejectWithValue('unauthorized! logging out')
       }
       return thunkAPI.rejectWithValue(error.response.data.msg)
