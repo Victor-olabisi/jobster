fd930ad3bdb017417b912544d6eeb06cec8d4c28
victor <oguntayovictor65@gmail.com>
2023-12-03 09:03:06 -0800

    minor fix

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,38 +2,42 @@ import Wrapper from "../assets/wrappers/Navbar";
 import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
 import Logo from "./Logo";
 import { useDispatch, useSelector } from "react-redux";
-import { toggleSidebar,logoutUser } from "../features/user/userSlice";
+import { toggleSidebar } from "../features/user/userSlice";
 import { useState } from "react";
 import { clearStore } from "../features/user/userSlice";
 
-
 const Navbar = () => {
-  const [showLogout, setShowLogout] = useState(false)
-  const { user } = useSelector((store) => store.user)
-  const dispatch = useDispatch()
+  const [showLogout, setShowLogout] = useState(false);
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
   // console.log(user.name);
 
   const toggle = () => {
-    dispatch(toggleSidebar())
-  }
+    dispatch(toggleSidebar());
+  };
   return (
     <Wrapper>
       <div className="nav-center">
         <button className="toggle-btn" onClick={toggle}>
-<FaAlignLeft/>
+          <FaAlignLeft />
         </button>
         <div>
           <Logo />
-          <h3>dashboard</h3>
+          <h3 className="hidden lg:block">dashboard</h3>
         </div>
         <div className="btn-container">
-          <button className="btn" onClick={()=>setShowLogout(!showLogout)}>
+          <button className="btn" onClick={() => setShowLogout(!showLogout)}>
             <FaUserCircle />
             {user?.name}
-            <FaCaretDown/>
+            <FaCaretDown />
           </button>
-          <div className={showLogout?'dropdown show-dropdown' : 'dropdown'}>
-            <button className="dropdown-btn" onClick={()=> dispatch(clearStore('logging out'))} >logout</button>
+          <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
+            <button
+              className="dropdown-btn"
+              onClick={() => dispatch(clearStore("logging out"))}
+            >
+              logout
+            </button>
           </div>
         </div>
       </div>
