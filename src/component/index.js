85f35e0810407375becd1418f05656aa925770fe
victor <oguntayovictor65@gmail.com>
2023-12-30 02:07:36 -0800

    fixing bugs

--- src/component/index.js
+++ src/component/index.js
@@ -1,8 +1,12 @@
-import Navbar from "./Navbar";
-import BigSidebar from "./BigSidebar";
-import SmallSidebar from "./SmallSidebar";
-// import JobContainer from "./JobContainer";
-import SearchContainer from "./searchContainer";
+// import Navbar from "./Navbar";
+// import BigSidebar from "./BigSidebar";
+// import SmallSidebar from "./SmallSidebar";
+// // import JobContainer from "./JobContainer";
+// import SearchContainer from "./searchContainer";
 
-export { Navbar, BigSidebar, SmallSidebar, SearchContainer }
-export {default as JobContainer} from './JobContainer'
+// export { Navbar, BigSidebar, SmallSidebar, SearchContainer }
+export { default as JobContainer } from './JobContainer'
+export { default as BigSidebar } from "./Bigsidebar";
+export { default as Navbar } from "./Navbar";
+export { default as SmallSidebar } from "./SmallSidebar";
+export { default as SearchContainer } from "./SearchContainer";
