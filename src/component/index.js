dd95f644949b9bb8198bc605d0821ab9d42912eb
victor <oguntayovictor65@gmail.com>
2023-12-30 02:03:02 -0800

    fixing bugs

--- src/component/index.js
+++ src/component/index.js
@@ -1,7 +1,8 @@
 import Navbar from "./Navbar";
 import BigSidebar from "./BigSidebar";
 import SmallSidebar from "./SmallSidebar";
-import JobContainer from "./JobContainer";
+// import JobContainer from "./JobContainer";
 import SearchContainer from "./searchContainer";
 
-export {Navbar, BigSidebar, SmallSidebar, JobContainer, SearchContainer}
+export { Navbar, BigSidebar, SmallSidebar, SearchContainer }
+export {default as JobContainer} from './JobContainer'
